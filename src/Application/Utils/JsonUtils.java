414736289d8a271d82dae210954279c63f22135f
AlfonsoG-dev <alfgomachi@gmail.com>
2024-12-16 16:09:53 -0500

    refactor: implement json types

--- src/Application/Utils/JsonUtils.java
+++ src/Application/Utils/JsonUtils.java
@@ -3,6 +3,16 @@ package Application.Utils;
 public class JsonUtils {
 
     public String getJsonType(String sentence) {
-        return "not implemented yet";
+        if(sentence.startsWith("\"")) {
+            return "String";
+        } else if(sentence.startsWith("[")) {
+            return "List";
+        } else if(sentence.startsWith("{")) {
+            return "Object";
+        } else if(sentence.equals("true") || sentence.equals("false")) {
+            return "Boolean";
+        } else {
+            return "Number";
+        }
     }
 }
